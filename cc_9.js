5c6d07357c738efebe4bc14f268314214fe944f6
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-23 19:44:25 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -17,3 +17,21 @@ class Employee {
 const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000)
 console.log(emp1.getDetails())
 console.log(emp1.calculateAnnualSalary())
+
+// Task 2 - Creating Manager Class
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary)
+        this.teamSize = teamSize
+    }
+    getDetails() {
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`
+    }
+    calculateBonus() {
+        return (this.salary * 12) * 0.1
+    }
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5)
+console.log(mgr1.getDetails())
+console.log(mgr1.calculateBonus())
